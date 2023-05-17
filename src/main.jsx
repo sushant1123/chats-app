5838b7561d99454c54743f7d33c076af84e0551a
Sushant Bahirat <sushantbahirat40@gmail.com>
2023-05-17 11:28:56 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+import App from "./App.jsx";
+
+// contexts
+import SettingsProvider from "./contexts/SettingsContext";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <SettingsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SettingsProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
